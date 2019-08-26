ed1265e6b95ab245f3d0cacbd63be386154c89f6
X1 Carbon <yjl9903@vip.qq.com>
2019-08-26 19:07:37 +0800

    add: bookinfo page

--- src/components/bookinfo.vue
+++ src/components/bookinfo.vue
@@ -1,35 +1,66 @@
 <template>
-  <v-container>
+  <v-container class="myInfo">
     <v-row>
       <v-breadcrumbs large :items="items" divider="/"></v-breadcrumbs>
     </v-row>
-    <v-row>
-      <v-col cols="3">
-        <div style="height:100%;max-height:200px;width:100%;background-color:red;"></div>
-        <!-- <v-img src=""></v-img> -->
-      </v-col>
-      <v-col cols="9">
-        <h1 class="display-1">{{ info.title }}</h1>
+    <v-card :loading="loading">
+      <v-card-title>{{ info.title }}</v-card-title>
+      <v-divider></v-divider>
+      <v-card-text>
         <v-row>
-          <v-col>作者: {{info.author}}</v-col>
-          <v-col>分类: {{info.category}}</v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-row>
+          <v-col cols="9">
+            <v-row>
+              <v-col><div class="table-key font-weight-bold">作者</div> {{ info.author }}</v-col>
+              <v-col><div class="font-weight-bold">规格</div> {{ info.format | formatF }}</v-col>
+            </v-row>
+            <v-row>
+              <v-col><div class="table-key font-weight-bold">出版社</div> {{ info.publisher }}</v-col>
+              <v-col><div class="font-weight-bold">ISBN</div> {{ info.isbn }}</v-col>
+            </v-row>
+            <v-row>
+              <v-col><div class="font-weight-bold">出版日期</div> {{ info.publicationDate }}</v-col>
+              <v-col><div class="font-weight-bold">邮寄重量</div> {{ info.shippingWeight }} 克</v-col>
+            </v-row>
+            <v-row>
+              <v-col><div class="font-weight-bold">语言</div> {{ info.language | lang }}</v-col>
+              <v-col><div class="font-weight-bold">展示价格</div> {{ info.listPrice }} 元</v-col>
+            </v-row>
+            <v-row>
+              <v-col><div class="font-weight-bold">分类</div> {{ info.category | kind }}</v-col>
+              <v-col><div class="font-weight-bold">售价</div> {{ info.ourPrice }} 元</v-col>
+            </v-row>
+            <v-row>
+              <v-col><div class="font-weight-bold">总页数</div> {{ info.numberOfPages }} 页</v-col>
+              <v-col><div class="font-weight-bold">库存量</div> {{ info.inStockNumber }} 本</v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="3">
 
-    </v-row>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="font-weight-bold">书籍简介</v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <vue-markdown class="markdown-body font-weight-regular black--text" :source="info.description"></vue-markdown>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
 import { getBookInfo } from '../common/bookservice'
+import { KindMap } from '../common/config'
 import { hasOwn } from '../util'
 
 export default {
   name: 'BookInfo',
   data: () => ({
-    info: {}
+    info: {}, loading: false
   }),
   computed: {
     items() {
@@ -50,11 +81,29 @@ export default {
   },
   methods: {
     async init(id) {
+      this.loading = true;
       let data = await getBookInfo(id);
       if (hasOwn(data, 'status') && data.status === 'error') {
         return ;
       }
       this.info = data;
+      this.loading = false;
+    }
+  },
+  filters: {
+    kind(val) {
+      if (hasOwn(KindMap, val)) return KindMap[val];
+      return '';
+    },
+    lang(val) {
+      if (val === 'en' || val === 'english') return '英语';
+      if (val === 'cn' || val === 'chinese') return '中文';
+      return '';
+    },
+    formatF(val) {
+      if (val === 'paperback') return '软装';
+      if (val === 'hardback') return '精装';
+      return '';
     }
   },
   beforeRouteUpdate(to, from, next) {
@@ -63,10 +112,18 @@ export default {
   },
   created() {
     this.init(this.$route.query.id);
+  },
+  components: {
+    VueMarkdown
   }
 }
 </script>
 
 <style>
+@import '~github-markdown-css';
 
+#myInfo .table-key {
+  display: inline-block;
+  width: 5em;
+}
 </style>
